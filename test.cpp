a2713a77e53d579dc7f90ae44649197e195cb0a7
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-04-25 15:21:24 +0800

    mengisi addnode

--- test.cpp
+++ test.cpp
@@ -11,7 +11,42 @@ Node* START = NULL;
 
 void addNode() 
 {
-    
+     int nim;
+     string   nama;
+     Node*   nodeBaru   = new Node();
+     cout  <<  "Masukkan   NIM:   ";
+     cin   >>  nim;
+     cout   <<  "Masukkan   Nama:   ";
+     cin   >>  nama;
+     nodeBaru->noMhs   =   nim;
+     nodeBaru->name  =   nama;
+
+             if (START   ==  NULL    []  nim   <=  START->noMhs)   
+             {
+                 if (START   !=  NULL   &&  nim   ==  START->noMhs) 
+                 {
+                    cout   <<  "NIM   sudah   ada"   <<  endl;
+                    return;
+                 }
+                 nodeBaru->next   =   START; 
+                 START=   nodeBaru; 
+                 return;
+             }
+
+        Node previous = START;
+        Node current = START;  
+        while ((current   !=   NULL)   &&  nim   >  (current->noMhs)) 
+        {
+            if (nim   ==  current->noMhs)
+            {
+                cout   <<  "NIM   sudah   ada"   <<  endl;
+                return;
+            }
+            previous   =  current;
+            current   =  current->next;
+        }
+         nodeBaru->next   =  current;
+         previous->next   =   nodeBaru;
 }
 
 
